230e12b8b897ae0402fba35bbf3f572cf339c011
aydinonac <aydinonac@yahoo.com>
2023-09-22 08:24:54 +0100

    update2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {useState, useEffect} from "react"
 export default function App() {
     const [categories, setCategories] = useState([])
     const [items, setItems] = useState([]);
+    const [selection, setSelection] = useState(null)
 
     useEffect(()=> {
         fetch ("http://localhost:3001/categories")
@@ -15,19 +16,17 @@ export default function App() {
         .then (response => response.json())
         .then (itemData => {setItems(itemData)})
     }, [])
-    useEffect(()=> {
-        fetch ("http://localhost:3001/items/id")
-        .then (response => response.json())
-        // .then ???
-    }, [])
     
     return (
         <div>
+            {/* Show Selected item */}
             <h1 style={{color: 'blue'}}> My Selection</h1>
             <hr></hr>
-            {/* ??? */}
+            {selection ? <h3 style={{color: 'purple'}}>
+                {'"' + selection.name + '"' + " by " + selection.description}</h3> :
+                <h2 style={{color: 'red'}}>Click a 'Select' button to choose an item</h2>}
             <hr></hr>
-
+            {/* List all categories */}
             <h1 style={{color: 'blue'}}> My Categories</h1>
             <hr></hr>
             {categories.map(category => (
@@ -37,7 +36,7 @@ export default function App() {
             )
             )} 
             <hr></hr>
-
+            {/* List all items */}
             <h1 style={{color: 'blue'}}> My Items</h1>
             <hr></hr>
             {items.map(item => (
@@ -47,9 +46,10 @@ export default function App() {
                     <p>{"Price: " + item.price}</p>
                     <p>{"Description: " + item.description}</p>
                     <p>{"Category: " + item.category.name}</p>
+                    <button style={{background:"turquoise"}} onClick ={ () => {setSelection(item)}}>Select</button>
                     <hr></hr>
                 </>
-            )
+                )
             )}      
         </div>
     )
